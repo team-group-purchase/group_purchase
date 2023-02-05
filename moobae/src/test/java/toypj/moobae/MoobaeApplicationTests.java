9f710882111bb309d80ddbc861915fd283406e11
SeongMon <kyn05005@gmail.com>
2023-02-05 23:27:54 +0900

    first setting

---
+++ moobae/src/test/java/toypj/moobae/MoobaeApplicationTests.java
@@ -0,0 +1,13 @@
+package toypj.moobae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoobaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
